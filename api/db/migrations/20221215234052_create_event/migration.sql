a2c9446196118aabe42a9ac1e015404a46c951dd
Matt Lewis <matt@mplewis.com>
2022-12-15 17:41:51 -0600

    feat: event model

---
+++ api/db/migrations/20221215234052_create_event/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "Event" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "email" TEXT NOT NULL,
+    "confirmed" BOOLEAN NOT NULL DEFAULT false,
+    "expiresAt" DATETIME NOT NULL,
+    "visible" BOOLEAN NOT NULL DEFAULT false,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "start" DATETIME NOT NULL,
+    "end" DATETIME NOT NULL,
+    "reminders" TEXT NOT NULL
+);
