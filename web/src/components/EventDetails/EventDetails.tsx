2462060725300e25dd8b80c211aa59489e1338a2
Matt Lewis <matt@mplewis.com>
2022-12-21 18:56:01 -0700

    wip: typography

--- web/src/components/EventDetails/EventDetails.tsx
+++ web/src/components/EventDetails/EventDetails.tsx
@@ -3,19 +3,16 @@ import dayjs from 'dayjs'
 import Calendar from '../Calendar/Calendar'
 
 interface Event {
-  id: 1
-  createdAt: '2021-12-19T23:17:51.355Z'
-  updatedAt: '2021-12-19T23:17:51.355Z'
-  token: 'n99XGgaIgAkx1okglvhNEPwnESmbs416'
-  confirmed: true
-  expiresAt: '2022-12-19T23:17:51.355Z'
-  visible: true
-  slug: 'my-event'
-  title: 'My Event'
-  description: 'Please come to my event!'
-  start: '2022-12-23T23:17:51.356Z'
-  end: '2022-12-24T01:17:51.356Z'
-  reminders: '1 day, 2 hours'
+  token: string
+  confirmed: boolean
+  expiresAt: string
+  visible: boolean
+  slug: string
+  title: string
+  description: string
+  start: string
+  end: string
+  reminders: string
 }
 
 interface Props {
@@ -31,10 +28,15 @@ const EventDetails = ({ event }: Props) => {
 
   return (
     <>
-      <pre>
-        <code>{JSON.stringify(event, null, 2)}</code>
-      </pre>
-      <Calendar month={month} day={day} time={time} />
+      <div className="flex gap-4 prose">
+        <div className="flex-initial">
+          <Calendar month={month} day={day} time={time} />
+        </div>
+        <div>
+          <h1>{event.title}</h1>
+          <p>{event.description}</p>
+        </div>
+      </div>
     </>
   )
 }
--- web/src/components/ViewEventCell/ViewEventCell.tsx
+++ web/src/components/ViewEventCell/ViewEventCell.tsx
@@ -4,6 +4,7 @@ import { ViewEventQuery } from 'types/graphql'
 import type { CellSuccessProps, CellFailureProps } from '@redwoodjs/web'
 
 import Calendar from '../Calendar/Calendar'
+import EventDetails from '../EventDetails/EventDetails'
 
 export const QUERY = gql`
   query ViewEventQuery($slug: String!) {
@@ -31,19 +32,6 @@ export const Failure = ({ error }: CellFailureProps) => (
   <div style={{ color: 'red' }}>Error: {error?.message}</div>
 )
 
-export const Success = ({ event }: CellSuccessProps<ViewEventQuery>) => {
-  const { start } = event
-  const s = dayjs(start)
-  const month = s.format('MMM')
-  const day = s.format('D')
-  const time = s.format('h:mm A')
-
-  return (
-    <>
-      <pre>
-        <code>{JSON.stringify(event, null, 2)}</code>
-      </pre>
-      <Calendar month={month} day={day} time={time} />
-    </>
-  )
-}
+export const Success = ({ event }: CellSuccessProps<ViewEventQuery>) => (
+  <EventDetails event={event} />
+)
