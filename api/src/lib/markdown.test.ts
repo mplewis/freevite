21a5a89b758c85440405c5c4e51115d1721cd753
Matt Lewis <matt@mplewis.com>
2023-02-23 00:51:09 -0700

    refactor: use different libraries for markdown + sanitization

---
+++ api/src/lib/markdown.test.ts
@@ -0,0 +1,28 @@
+import { markdownToHTML } from './markdown'
+
+describe('markdownToHTML', () => {
+  it('sanitizes the HTML', async () => {
+    const input = `
+# Hello, world!
+
+This is some prose.
+
+[Link to a website](https://example.com)
+
+Bracket fish: <>< ><>
+
+<script>alert("Uh-oh!")</script>
+
+<img src="shouldnt-render.png">
+    `.trim()
+    expect(markdownToHTML(input)).toMatchInlineSnapshot(`
+      "<h1>Hello, world!</h1>
+      <p>This is some prose.</p>
+      <p><a target="_blank" href="https://example.com">Link to a website</a></p>
+      <p>Bracket fish: &lt;&gt;&lt; &gt;&lt;&gt;</p>
+
+
+      "
+    `)
+  })
+})
--- api/src/lib/markdown.ts
+++ api/src/lib/markdown.ts
@@ -1,37 +1,29 @@
-import { Node } from 'unist'
+import { marked } from 'marked'
+import sanitize from 'sanitize-html'
 
-const unistUtilVisit = import('unist-util-visit')
-const rehypeFilter = import('react-markdown/lib/rehype-filter.js')
-const rehypeStringify = import('rehype-stringify')
-const remarkParse = import('remark-parse')
-const remarkRehype = import('remark-rehype')
-const Unified = import('unified')
+const allowedTags = [
+  'a',
+  'blockquote',
+  'code',
+  'em',
+  'h1',
+  'h2',
+  'h3',
+  'h4',
+  'h5',
+  'h6',
+  'hr',
+  'li',
+  'ol',
+  'p',
+  'pre',
+  'strong',
+  'ul',
+]
 
-interface HTMLNode extends Node {
-  tagName: string
-  properties: Record<string, string>
-}
-
-async function addTargetBlank() {
-  const { visit } = await unistUtilVisit
-  return (tree: Node) => {
-    visit(tree, 'element', (node: HTMLNode) => {
-      if (node.tagName !== 'a') return
-      node.properties['target'] = '_blank'
-    })
-  }
-}
-
-export async function markdownToHTML(markdown: string): Promise<string> {
-  const allowedElements = ['a', 'p', 'ul', 'li', 'strong', 'em', 'hr', 'br']
-  const unified = (await Unified).unified
-  const v = await unified()
-    .use((await remarkParse).default)
-    .use((await remarkRehype).default)
-    .use((await rehypeStringify).default)
-    .use((await rehypeFilter).default, { allowedElements })
-    // @ts-expect-error HACK: types don't match here; fix this along with the ESM stuff
-    .use(addTargetBlank)
-    .process(markdown)
-  return String(v)
+export function markdownToHTML(markdown: string): string {
+  const html = marked.parse(markdown)
+  const clean = sanitize(html, { allowedTags })
+  const newWindows = clean.replace(/<a /g, '<a target="_blank" ')
+  return newWindows
 }
