10b80bfdb7689189bf22785a2b69103d580f8c82
Matt Lewis <matt@mplewis.com>
2023-02-23 01:34:50 -0700

    feat: use DEPLOY_PRIME_URL for site host

--- app.config.ts
+++ app.config.ts
@@ -1,1 +1,11 @@
-export const SITE_HOST = process.env.SITE_HOST
+const extractAfterHTTPSMatcher = /^https:\/\/(.*)/
+function extractAfterHTTPS(url?: string) {
+  if (!url) return url
+  const match = url.match(extractAfterHTTPSMatcher)
+  if (match) return match[1]
+  return url
+}
+
+export const SITE_HOST =
+  extractAfterHTTPS(process.env.DEPLOY_PRIME_URL) || // Netlify deploy preview
+  process.env.SITE_HOST // production
