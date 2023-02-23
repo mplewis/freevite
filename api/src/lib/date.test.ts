0d42be64486d189893118d71a8b6add1ce1611e2
Matt Lewis <matt@mplewis.com>
2023-02-22 23:08:47 -0700

    fix: refactor test

--- api/src/lib/date.test.ts
+++ api/src/lib/date.test.ts
@@ -3,11 +3,11 @@ import { convertToDateArray } from './date'
 describe('convertToDateArray', () => {
   it('converts dates as expected', () => {
     const examples = [
-      [['2020-01-01T00:00:00Z'], [2020, 1, 1, 0, 0]],
-      [['2020-01-01T00:00:00.000Z'], [2020, 1, 1, 0, 0]],
-      [['2020-01-01T00:00:00-06:00'], [2020, 1, 1, 6, 0]],
+      { in: '2020-01-01T00:00:00Z', out: [2020, 1, 1, 0, 0] },
+      { in: '2020-01-01T00:00:00.000Z', out: [2020, 1, 1, 0, 0] },
+      { in: '2020-01-01T00:00:00-06:00', out: [2020, 1, 1, 6, 0] },
     ]
-    for (const example of examples)
-      expect(convertToDateArray(new Date(example[0]))).toEqual(example[1])
+    for (const e of examples)
+      expect(convertToDateArray(new Date(e.in))).toEqual(e.out)
   })
 })
