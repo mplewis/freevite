a5403721c08c0aa86fd20271c534c0ce9b16acb5
Matt Lewis <matt@mplewis.com>
2023-02-26 14:58:44 -0700

    fix: unit tests

--- api/src/lib/email/send.ts
+++ api/src/lib/email/send.ts
@@ -1,6 +1,7 @@
 import * as nodemailer from 'nodemailer'
 
 import { MAIL_CONFIG, MAIL_SENDER } from 'src/api.config'
+import { CI } from 'src/app.config'
 
 interface Params {
   to: string | string[]
@@ -11,9 +12,9 @@ interface Params {
 const transport = nodemailer.createTransport(MAIL_CONFIG)
 
 export async function sendEmail({ subject, text, ...p }: Params) {
+  if (CI) return
   const { name, email } = MAIL_SENDER
   const from = `"${name}" <${email}>`
   const to = Array.isArray(p.to) ? p.to : [p.to]
-  console.log('sending', { subject, text, from, to })
   return transport.sendMail({ subject, text, from, to })
 }
--- app.config.ts
+++ app.config.ts
@@ -13,3 +13,5 @@ const context: NetlifyContext = process.env.CONTEXT as NetlifyContext
 export let SITE_HOST = process.env.SITE_HOST
 if (context === 'deploy-preview' || context === 'branch-deploy')
   SITE_HOST = extractAfterHTTPS(process.env.DEPLOY_PRIME_URL)
+
+export const CI = process.env.NODE_ENV === 'test'
