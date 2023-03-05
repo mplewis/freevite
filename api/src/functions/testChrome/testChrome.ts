a1a57bda66531b0ae1cfa4c15cc2b6c97ec941d6
Matt Lewis <matt@mplewis.com>
2023-03-05 16:21:35 -0700

    wip

--- api/src/functions/testChrome/testChrome.ts
+++ api/src/functions/testChrome/testChrome.ts
@@ -4,8 +4,23 @@ import { join } from 'path'
 import chromium from '@sparticuz/chromium'
 import type { APIGatewayEvent, Context } from 'aws-lambda'
 import puppeteer from 'puppeteer-core'
+// import { chromium as playwright } from 'playwright'
 
-import indexHtml from './assets/index.html'
+const indexHtml = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>My Preview Page</title>
+</head>
+<body>
+	<h1>This is a preview page</h1>
+	<p>And here is some preview text</p>
+</body>
+</html>
+`
 
 const ogImageSize = { width: 1200, height: 630 }
 
@@ -17,16 +32,22 @@ export const handler = async (_event: APIGatewayEvent, _context: Context) => {
     args: chromium.args,
     defaultViewport: ogImageSize,
     headless: true,
+    // dumpio: true,
   })
 
   const page = await browser.newPage()
   await page.setContent(indexHtml, { waitUntil: 'domcontentloaded' })
-  const screenshot = await page.screenshot()
+  // await page.goto('https://example.com', { waitUntil: 'domcontentloaded' })
+  const screenshot = await page.screenshot({ path: '/tmp/screenshot.png' })
+  const title = await page.title()
+  await browser.close()
 
   return {
     statusCode: 200,
     headers: { 'Content-Type': 'image/png' },
     body: screenshot.toString('base64'),
     isBase64Encoded: true,
+    // headers: { 'Content-Type': 'text/plain' },
+    // body: title,
   }
 }
