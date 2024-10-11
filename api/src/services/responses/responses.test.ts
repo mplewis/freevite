23ffd6939e4ed92a427b69ef376950d4c7ee7c8d
Matt Lewis <matt@mplewis.com>
2024-10-11 13:53:49 -0600

    feat: create/delete reminders

--- api/src/services/responses/responses.test.ts
+++ api/src/services/responses/responses.test.ts
@@ -1,4 +1,8 @@
 import type { Response } from '@prisma/client'
+import dayjs from 'dayjs'
+import duration from 'dayjs/plugin/duration'
+
+import { db } from 'src/lib/db'
 
 import {
   responses,
@@ -9,11 +13,7 @@ import {
 } from './responses'
 import type { StandardScenario } from './responses.scenarios'
 
-// Generated boilerplate tests do not account for all circumstances
-// and can fail without adjustments, e.g. Float.
-//           Please refer to the RedwoodJS Testing Docs:
-//       https://redwoodjs.com/docs/testing#testing-services
-// https://redwoodjs.com/docs/testing#jest-expect-type-considerations
+dayjs.extend(duration)
 
 describe('responses', () => {
   scenario('returns all responses', async (scenario: StandardScenario) => {
@@ -25,7 +25,7 @@ describe('responses', () => {
   scenario('returns a single response', async (scenario: StandardScenario) => {
     const result = await response({ id: scenario.response.one.id })
 
-    expect(result).toEqual(scenario.response.one)
+    expect(result).toEqual({ ...scenario.response.one, reminders: [] })
   })
 
   scenario('creates a response', async (scenario: StandardScenario) => {
@@ -42,7 +42,35 @@ describe('responses', () => {
     expect(result.eventId).toEqual(scenario.response.two.eventId)
     expect(result.email).toEqual('jane@example.com')
     expect(result.headCount).toEqual(1)
+    expect(result.reminders.length).toEqual(0)
   })
+
+  // TODO: Updating event updates reminders
+
+  scenario(
+    'creates a response with a reminder',
+    async (scenario: StandardScenario) => {
+      const result = await createResponse({
+        eventId: scenario.response.two.eventId,
+        input: {
+          name: 'Jane Doe',
+          email: 'jane@example.com',
+          headCount: 1,
+          comment: "I'll be there!",
+          remindPriorSec: dayjs.duration(1, 'day').asSeconds(),
+        },
+      })
+      const event = await db.event.findUnique({ where: { id: result.eventId } })
+
+      expect(result.eventId).toEqual(scenario.response.two.eventId)
+      expect(result.email).toEqual('jane@example.com')
+      expect(result.headCount).toEqual(1)
+      expect(result.reminders.length).toEqual(1)
+      expect(dayjs(event.start).diff(result.reminders[0].sendAt)).toEqual(
+        dayjs.duration(1, 'day').asMilliseconds()
+      )
+    }
+  )
 
   scenario('updates a response', async (scenario: StandardScenario) => {
     const original = (await response({
@@ -63,5 +91,33 @@ describe('responses', () => {
     const result = await response({ id: original.id })
 
     expect(result).toEqual(null)
+  })
+
+  scenario('adds a reminder', async (scenario: StandardScenario) => {
+    const original = (await response({
+      id: scenario.response.one.id,
+    })) as Response
+    const result = await updateResponse({
+      editToken: original.editToken,
+      input: { remindPriorSec: dayjs.duration(1, 'day').asSeconds() },
+    })
+    const event = await db.event.findUnique({ where: { id: original.eventId } })
+
+    expect(result.reminders.length).toEqual(1)
+    expect(dayjs(event.start).diff(result.reminders[0].sendAt)).toEqual(
+      dayjs.duration(1, 'day').asMilliseconds()
+    )
+  })
+
+  scenario('deletes a reminder', async (scenario: StandardScenario) => {
+    const original = (await response({
+      id: scenario.response.one.id,
+    })) as Response
+    const result = await updateResponse({
+      editToken: original.editToken,
+      input: { remindPriorSec: null },
+    })
+
+    expect(result.reminders.length).toEqual(0)
   })
 })
--- api/src/services/responses/responses.ts
+++ api/src/services/responses/responses.ts
@@ -1,3 +1,4 @@
+import dayjs from 'dayjs'
 import type {
   QueryResolvers,
   MutationResolvers,
@@ -8,13 +9,11 @@ import { db } from 'src/lib/db'
 import { generateToken } from 'src/lib/token'
 
 export const responses: QueryResolvers['responses'] = () => {
-  return db.response.findMany()
+  return db.response.findMany({ include: { reminders: true } })
 }
 
 export const response: QueryResolvers['response'] = ({ id }) => {
-  return db.response.findUnique({
-    where: { id },
-  })
+  return db.response.findUnique({ where: { id }, include: { reminders: true } })
 }
 
 export const createResponse: MutationResolvers['createResponse'] = async ({
@@ -23,11 +22,23 @@ export const createResponse: MutationResolvers['createResponse'] = async ({
 }) => {
   const event = await db.event.findUnique({ where: { id: eventId } })
   if (!event) throw new Error(`Event not found: ${eventId}`)
+
+  const reminders: { sendAt: Date }[] = []
+  if (input.remindPriorSec) {
+    const sendAt = dayjs(event.start)
+      .subtract(input.remindPriorSec, 'second')
+      .toDate()
+    reminders.push({ sendAt })
+  }
+  delete input.remindPriorSec
+
   return db.response.create({
+    include: { reminders: true },
     data: {
       event: { connect: { id: event.id } },
       editToken: generateToken(),
       ...input,
+      reminders: { create: reminders },
     },
   })
 }
@@ -36,7 +47,33 @@ export const updateResponse: MutationResolvers['updateResponse'] = ({
   editToken,
   input,
 }) => {
-  return db.response.update({ data: input, where: { editToken } })
+  const { remindPriorSec, ...rest } = input
+
+  return db.$transaction(async (tx) => {
+    const response = await tx.response.update({
+      data: rest,
+      where: { editToken },
+    })
+    const event = await tx.event.findUnique({ where: { id: response.eventId } })
+    if (!event) throw new Error(`Event not found: ${response.eventId}`)
+
+    await tx.reminder.deleteMany({ where: { responseId: response.id } })
+    if (remindPriorSec) {
+      await tx.reminder.create({
+        data: {
+          response: { connect: { id: response.id } },
+          sendAt: dayjs(event.start)
+            .subtract(input.remindPriorSec, 'second')
+            .toDate(),
+        },
+      })
+    }
+
+    return tx.response.findUnique({
+      where: { editToken },
+      include: { reminders: true },
+    })
+  })
 }
 
 export const deleteResponse: MutationResolvers['deleteResponse'] = ({
