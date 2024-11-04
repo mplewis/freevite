26cc2cc322da2c09f05e16068c72cce36812b321
Matt Lewis <matt@mplewis.com>
2024-11-03 22:23:02 -0700

    HACK: try console log too

--- api/src/functions/sendReminders/sendReminders.ts
+++ api/src/functions/sendReminders/sendReminders.ts
@@ -4,8 +4,10 @@ import { logger } from 'src/lib/logger'
 import { sendOutstandingReminders } from 'src/lib/schedule/reminder'
 
 export const handler = async (_event: APIGatewayEvent, _context: Context) => {
+  console.log('console.log: start sendReminders')
   logger.info('Start: sendOutstandingReminders')
   await sendOutstandingReminders()
+  console.log('console.log: finish sendReminders')
   logger.info('Finish: sendOutstandingReminders')
   return { statusCode: 200 }
 }
