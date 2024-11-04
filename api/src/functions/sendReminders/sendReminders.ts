94bad51ee04bd7aa0bcbf9f55d5e557828c0e9aa
Matt Lewis <matt@mplewis.com>
2024-11-03 22:30:02 -0700

    fix: log level for prod

--- api/src/functions/sendReminders/sendReminders.ts
+++ api/src/functions/sendReminders/sendReminders.ts
@@ -1,13 +1,8 @@
 import type { APIGatewayEvent, Context } from 'aws-lambda'
 
-import { logger } from 'src/lib/logger'
 import { sendOutstandingReminders } from 'src/lib/schedule/reminder'
 
 export const handler = async (_event: APIGatewayEvent, _context: Context) => {
-  console.log('console.log: start sendReminders')
-  logger.info('Start: sendOutstandingReminders')
   await sendOutstandingReminders()
-  console.log('console.log: finish sendReminders')
-  logger.info('Finish: sendOutstandingReminders')
   return { statusCode: 200 }
 }
