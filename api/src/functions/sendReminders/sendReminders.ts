4e34de416fad6996700e035bfb18cb091daa4a69
Matt Lewis <matt@mplewis.com>
2024-11-03 22:22:31 -0700

    fix: try to get logging working

--- api/src/functions/sendReminders/sendReminders.ts
+++ api/src/functions/sendReminders/sendReminders.ts
@@ -1,8 +1,11 @@
 import type { APIGatewayEvent, Context } from 'aws-lambda'
 
+import { logger } from 'src/lib/logger'
 import { sendOutstandingReminders } from 'src/lib/schedule/reminder'
 
 export const handler = async (_event: APIGatewayEvent, _context: Context) => {
+  logger.info('Start: sendOutstandingReminders')
   await sendOutstandingReminders()
+  logger.info('Finish: sendOutstandingReminders')
   return { statusCode: 200 }
 }
