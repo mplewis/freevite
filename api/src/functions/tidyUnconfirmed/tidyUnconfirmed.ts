7876e3ed2de459d889c02d11392a10ab3f26e86c
Matt Lewis <matt@mplewis.com>
2024-11-13 23:09:05 -0700

    Tidy unconfirmed events/responses (dry run) (#38)
    List unconfirmed and stale events and responses that would be deleted
    when this feature goes live.

---
+++ api/src/functions/tidyUnconfirmed/tidyUnconfirmed.ts
@@ -0,0 +1,9 @@
+import type { APIGatewayEvent, Context } from 'aws-lambda'
+
+import { tidyUnconfirmedEvents, tidyUnconfirmedResponses } from 'src/lib/tidy'
+
+export const handler = async (_event: APIGatewayEvent, _context: Context) => {
+  await tidyUnconfirmedEvents()
+  await tidyUnconfirmedResponses()
+  return { statusCode: 200 }
+}
