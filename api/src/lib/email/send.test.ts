69ff3295a4d9f9fa214a903e6272da7a019105b3
Matt Lewis <matt@mplewis.com>
2024-11-19 23:50:49 -0700

    feat: obey the ignore list

--- api/src/lib/email/send.test.ts
+++ api/src/lib/email/send.test.ts
@@ -1,12 +1,17 @@
 import { InMemoryMailHandler } from '@redwoodjs/mailer-handler-in-memory'
 
+import { db } from '../db'
 import { mailer } from '../mailer'
 
 import { sendEmail } from './send'
 
 describe('mailer', () => {
+  const testHandler = mailer.getTestHandler() as InMemoryMailHandler
+  beforeEach(async () => {
+    await testHandler.clearInbox()
+  })
+
   it('sends an email', async () => {
-    const testHandler = mailer.getTestHandler() as InMemoryMailHandler
     expect(testHandler.inbox).toHaveLength(0)
 
     const params = {
@@ -41,5 +46,32 @@ https://example.com/unsubscribe?email=darlene%40fs0ciety.pizza&token=wgPFf2G9H7D
   ],
 }
 `)
+  })
+
+  describe('when users are on the ignore list', () => {
+    beforeEach(async () => {
+      await db.ignoredEmail.create({ data: { email: 'wh1ter0se@da.cn' } })
+    })
+    afterEach(async () => {
+      await db.ignoredEmail.deleteMany()
+    })
+
+    it('obeys the ignore list', async () => {
+      expect(testHandler.inbox).toHaveLength(0)
+      await sendEmail({
+        to: 'wh1ter0se@da.cn',
+        subject: "You're invited to End of the World Party",
+        text: 'Bring your own mask',
+      })
+      expect(testHandler.inbox).toHaveLength(0)
+
+      // but it still sends to other addresses
+      await sendEmail({
+        to: 'ealderson@ecorp.com',
+        subject: 'Looking for an update on the malware presentation',
+        text: 'Please send ASAP',
+      })
+      expect(testHandler.inbox).toHaveLength(1)
+    })
   })
 })
--- api/src/lib/email/send.ts
+++ api/src/lib/email/send.ts
@@ -1,6 +1,7 @@
 import { MAIL_SENDER } from 'src/api.config'
 import { mailer } from 'src/lib/mailer'
 
+import { db } from '../db'
 import { logger } from '../logger'
 
 import { Plain } from './plain'
@@ -18,12 +19,33 @@ interface Params {
  * @returns The result of the send operation
  */
 export async function sendEmail({ subject, text: _text, ...p }: Params) {
-  const firstRecipient = Array.isArray(p.to) ? p.to[0] : p.to
-  const to = Array.isArray(p.to) ? p.to : [p.to]
+  const unfilteredTo = Array.isArray(p.to) ? p.to : [p.to]
+  const to = await minusIgnored(unfilteredTo)
+  if (to.length === 0) return
 
+  const firstRecipient = Array.isArray(p.to) ? p.to[0] : p.to
   const text = _text.trim() + '\n\n' + unsubscribeFooter(firstRecipient)
   const { name, email } = MAIL_SENDER
   const from = `"${name}" <${email}>`
   logger.info({ from, to, subject, text }, 'Sending email')
   return mailer.send(Plain({ text }), { subject, from, to })
+}
+
+/** Filter a list of emails to remove recipients who have opted out of emails. */
+async function minusIgnored(to: string[]): Promise<string[]> {
+  const result = await db.ignoredEmail.findMany({
+    where: { email: { in: to } },
+    select: { email: true },
+  })
+  const ignored = result.map((x) => x.email).filter(Boolean) as string[]
+
+  if (ignored.length > 0) {
+    logger.info(
+      { emailsOnIgnoreList: ignored },
+      'Refusing to email user(s) on ignore list'
+    )
+  }
+
+  const allowed = to.filter((email) => !ignored.includes(email))
+  return allowed
 }
